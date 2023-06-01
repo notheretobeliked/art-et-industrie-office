75a8ae79bb43ce9e8acbb18b9f77f60e21f950d7
Erik Hartin <erik@sans-titre.net>
2023-06-01 09:14:51 +0100

    Fix to gradient 

--- web/app/themes/nhtbl-sage/resources/views/sections/header.blade.php
+++ web/app/themes/nhtbl-sage/resources/views/sections/header.blade.php
@@ -65,7 +65,7 @@
     <div class="w-full h-screen relative">
       <svg width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1440 920" fill="none"
         xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_202_5603)">
+        <g clip-path="url(#clip0_202_56030)">
           <rect width="1440" height="920" fill="#5A8BFF" />
           <path
             d="M503.536 -22.3029C524.503 10.3564 529.862 57.1264 509.474 97.7493C489.087 138.372 397.115 253.26 369.92 275.384C342.724 297.509 342.7 333.338 415.616 334.876C488.532 336.415 533.556 341.999 513.396 393.228C493.237 444.457 388.618 569.09 283.782 594.811C208.196 613.331 -341.204 518.537 -56.9393 61.5356C87.3328 -170.413 457.161 -94.4951 503.536 -22.3029Z"
@@ -79,7 +79,7 @@
           <g mask="url(#mask0_202_5603)">
             <path
               d="M307.999 603.051C621.529 326.834 676.049 -198.851 429.772 -571.098C183.496 -943.345 -270.317 -1021.19 -583.847 -744.975C-897.377 -468.758 -951.896 56.9264 -705.62 429.174C-459.343 801.421 -5.53047 879.268 307.999 603.051Z"
-              fill="url(#paint0_radial_202_5603)" />
+              fill="url(#paint0_radial_202_56030)" />
           </g>
           <path
             d="M1422.07 -56.0025C1422.07 -56.0025 1176.08 -71.1486 1114.58 42.4736C1053.08 156.096 1008.61 302.76 914.005 353.164C819.394 403.568 551.47 489.308 513.45 553.8C479.974 610.592 489.771 875.744 458.447 923.762C435.874 958.317 279.731 1131.09 386.267 1186.67C492.803 1242.25 1255.79 1283.85 1404.84 1186.36C1682.81 1004.52 2054.26 24.802 1422.07 -56.0025Z"
@@ -93,24 +93,24 @@
           <g mask="url(#mask1_202_5603)">
             <path
               d="M1913.13 2033.41C2362.74 1680.94 2426.13 989.948 2054.72 490.032C1683.3 -9.88513 1017.72 -129.414 568.104 223.056C118.49 575.526 55.1005 1266.52 426.52 1766.44C797.939 2266.36 1463.52 2385.88 1913.13 2033.41Z"
-              fill="url(#paint1_radial_202_5603)" />
+              fill="url(#paint1_radial_202_56030)" />
           </g>
           <path
             d="M831.251 1194.24C1065.44 1150.02 1219.66 914.718 1175.7 668.681C1131.74 422.643 906.252 259.037 672.059 303.257C437.865 347.476 283.65 582.776 327.61 828.814C371.57 1074.85 597.057 1238.46 831.251 1194.24Z"
-            fill="url(#paint2_radial_202_5603)" />
-          <g filter="url(#filter0_b_202_5603)">
+            fill="url(#paint2_radial_202_56030)" />
+          <g filter="url(#filter0_b_202_56034)">
             <rect width="1440" height="920" fill="white" fill-opacity="0.01" />
           </g>
         </g>
         <defs>
-          <filter id="filter0_b_202_5603" x="-164" y="-164" width="1768" height="1248"
+          <filter id="filter0_b_202_56034" x="-164" y="-164" width="1768" height="1248"
             filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
             <feFlood flood-opacity="0" result="BackgroundImageFix" />
             <feGaussianBlur in="BackgroundImageFix" stdDeviation="82" />
             <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_202_5603" />
             <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_202_5603" result="shape" />
           </filter>
-          <radialGradient id="paint0_radial_202_5603" cx="0" cy="0" r="1"
+          <radialGradient id="paint0_radial_202_56030" cx="0" cy="0" r="1"
             gradientUnits="userSpaceOnUse"
             gradientTransform="translate(-136.122 -60.5628) rotate(-41.3798) scale(757.529 802.398)">
             <stop offset="0.19" stop-color="#FF4D0D" />
@@ -118,7 +118,7 @@
             <stop offset="0.65" stop-color="#E4F05D" />
             <stop offset="1" stop-color="#8CFDCC" stop-opacity="0" />
           </radialGradient>
-          <radialGradient id="paint1_radial_202_5603" cx="0" cy="0" r="1"
+          <radialGradient id="paint1_radial_202_56030" cx="0" cy="0" r="1"
             gradientUnits="userSpaceOnUse"
             gradientTransform="translate(1242.42 1130.91) rotate(53.3891) scale(1126.6 1033.48)">
             <stop offset="0.19" stop-color="#FF4D0D" />
@@ -126,14 +126,14 @@
             <stop offset="0.65" stop-color="#E4F05D" />
             <stop offset="1" stop-color="#8CFDCC" stop-opacity="0" />
           </radialGradient>
-          <radialGradient id="paint2_radial_202_5603" cx="0" cy="0" r="1"
+          <radialGradient id="paint2_radial_202_56030" cx="0" cy="0" r="1"
             gradientUnits="userSpaceOnUse"
             gradientTransform="translate(759.811 749.16) rotate(79.8698) scale(452.353 432.934)">
             <stop offset="0.19" stop-color="#FF4D0D" />
             <stop offset="0.52" stop-color="#FBEC40" />
             <stop offset="0.99" stop-color="#8CFDCC" stop-opacity="0" />
           </radialGradient>
-          <clipPath id="clip0_202_5603">
+          <clipPath id="clip0_202_56030">
             <rect width="1440" height="920" fill="white" />
           </clipPath>
         </defs>
