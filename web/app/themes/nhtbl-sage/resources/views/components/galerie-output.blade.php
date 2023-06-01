ccf5bfbcf78613ab24b8c5f14af9c168bd55dac6
Erik Hartin <erik@sans-titre.net>
2023-06-01 09:17:00 +0100

    fix height on galeries

--- web/app/themes/nhtbl-sage/resources/views/components/galerie-output.blade.php
+++ web/app/themes/nhtbl-sage/resources/views/components/galerie-output.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full overflow-x-scroll hide-scrollbar h-96 md:h-132 gap-4 scroll-container overflow-y-hidden">
+<div class="w-full overflow-x-scroll hide-scrollbar h-96 md:h-136 gap-4 scroll-container overflow-y-hidden">
   <div class="flex flex-row h-96 md:h-132 gap-4 flex-grow">
     @foreach ($images as $image)
       <x-image-output :image="$image" size="large" caption class=" h-80 md:h-128" />
